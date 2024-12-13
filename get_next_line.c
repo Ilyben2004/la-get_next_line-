19eb538144e4f537b81093215c5c359a2f7cd92e
Ilyas Bennajeh <ibennaje@c3r4p1.1337.ma>
2024-12-13 14:45:47 +0100

    in new line not included

--- get_next_line.c
+++ get_next_line.c
@@ -5,10 +5,15 @@ char *get_next_line(int fd)
     static char reminder[BUFFER_SIZE];
     char *buffer;
     int read_ret;
+    char *line;
 
     buffer = malloc (BUFFER_SIZE);
     read_ret = read(fd, buffer, BUFFER_SIZE);
-    if
+    if (line = extract_new_line_reminder(buffer , reminder))
+        return (line);
+    else
+        
+
 
 
 
